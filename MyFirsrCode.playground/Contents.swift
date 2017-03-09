68e657f5e8929e07b16e8e196eec148c0447cdee
MacBook Pro <Mohaboelwafa1@gmail.com>
2017-03-09 11:24:32 +0200

    Upload 1

---
+++ MyFirsrCode.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "\0"
+
+var x : Int
+
+x = 10
+
+x+=1
+
+var a = 10
+
+a = 20
+
+var yu : Int?
+
+print(str)
+
+
+if(x != 10){
+
+print(a)
+}else{
+print(20)
+}
+var abc = "20d"
+
+print (("welcome\(a)"+"goodbye").isEmpty)
+print (abc.isEmpty)
+
